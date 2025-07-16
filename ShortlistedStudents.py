2fb2ff5111335bcbedb1267d9a0d7665240089d3
ashreyanair <arunshreyanair@gmail.com>
2025-07-17 00:53:57 +0530

    Update ShortlistedStudents.py

--- ShortlistedStudents.py
+++ ShortlistedStudents.py
@@ -2,6 +2,6 @@ notorious=["Darshan","Anuj","Rohit"]
 names=["Anu","Darshan","Rohit","Jack","Priya","Bala","Rai","Ram","Raj","Benscore"]
 score=[2,5,6,8,3,5,6,9,8,2]
 for i in range(len(names)):
-    if score[i]-5:
+    if score[i]>5:
         if names[i] not in notorious:
             print(names[i])
